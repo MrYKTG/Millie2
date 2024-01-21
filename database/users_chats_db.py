e9f054019855b00a6ea2cf834bd9e8f73a95ffdc
Mr kuttusan <yedhukrishnanu2001@gmail.com>
2024-01-21 19:23:40 +0530

    Update users_chats_db.py
    🕸️

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -1,7 +1,7 @@
 
 # https://github.com/odysseusmax/animated-lamp/blob/master/bot/database/database.py
 import motor.motor_asyncio
-from info import DATABASE_NAME, DATABASE_URI, IMDB, IMDB_TEMPLATE, MELCOW_NEW_USERS, P_TTI_SHOW_OFF, SINGLE_BUTTON, SPELL_CHECK_REPLY, PROTECT_CONTENT, AUTO_DELETE, SHORTLINK_API, SHORTLINK_URL, IS_SHORTLINK
+from info import DATABASE_NAME, DATABASE_URI, IMDB, IMDB_TEMPLATE, MELCOW_NEW_USERS, P_TTI_SHOW_OFF, SINGLE_BUTTON, SPELL_CHECK_REPLY, PROTECT_CONTENT, AUTO_DELETE
 
 class Database:
     
@@ -116,11 +116,7 @@ class Database:
             'spell_check': SPELL_CHECK_REPLY,
             'welcome': MELCOW_NEW_USERS,
             'auto_delete': AUTO_DELETE,
-            'template': IMDB_TEMPLATE,
-            'shortlink': SHORTLINK_URL,
-            'shortlink_api': SHORTLINK_API,
-            'is_shortlink': IS_SHORTLINK
-    
+            'template': IMDB_TEMPLATE
         }
         chat = await self.grp.find_one({'id':int(id)})
         if chat:
